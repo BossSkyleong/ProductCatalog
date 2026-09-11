f2546b0fb18e9cb86d53a756c3cc968c972d4623
BossSkyleong <bossleongzhicong@gmail.com>
2026-09-11 12:39:59 +0800

    Add ProductRepository using ProductApi

---
+++ app/src/main/java/com/example/neurogineproductcatalog/data/repository/ProductRepository.java
@@ -0,0 +1,31 @@
+package com.example.neurogineproductcatalog.data.repository;
+
+import com.example.neurogineproductcatalog.data.api.ProductApi;
+import com.example.neurogineproductcatalog.data.model.ProductResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class ProductRepository {
+
+    private final ProductApi productApi;
+
+    public ProductRepository() {
+        productApi = ProductApi.Client.getProductApi();
+    }
+
+    public void getProducts(int limit, int skip, Callback<ProductResponse> callback){
+        Call<ProductResponse> call = productApi.getProducts(limit, skip);
+        call.enqueue(callback);
+    }
+
+    public void getProductById(int id, Callback<ProductResponse> callback){
+        Call<ProductResponse> call = productApi.getProductById(id);
+        call.enqueue(callback);
+    }
+
+    public void searchProducts(String query, Callback<ProductResponse> callback){
+        Call<ProductResponse> call = productApi.searchProducts(query);
+        call.enqueue(callback);
+    }
+}
